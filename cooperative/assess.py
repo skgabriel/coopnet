a07e398497abc3065be1251727c18c6cebf2d964
skgabriel <skgabriel@users.noreply.github.com>
2021-05-02 13:45:16 -0700

    Update assess.py

--- cooperative/assess.py
+++ cooperative/assess.py
@@ -3,18 +3,25 @@ import numpy as np
 import ast
 from operator import add
 
-subset = 'aan'
-model = 'gpt2' #lstm #'gpt2'
-key_ = '_full'
-if model == 'lstm':
-   key_ = '_lstm'
-gen_folder = './final_eval_gens/' + model + '/' + subset + '/'
-source_folder = './final_eval_gens/' + subset + '_gens' + key_
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--subset',type=str,default='aan')
+    parser.add_argument('--gen_folder',type=str,default='./gens')
+    parser.add_argument('--source_folder', type=str, default='./sources')
+    parser.add_argument('--aan_model_path',type=str,default='./aan_model/model/best_params') 
+    parser.add_argument('--bio_model_path',type=str,default='./bio_model/model/best_params')
+    parser.add_argument('--gen_model_path',type=str,default='./gen_model/aan/model/best_params')
+    parser.add_argument('--topk',type=int,default=3)
+    parser.add_argument('--num_cands',type=int,default=10)
+    parser.add_argument('--split',type=str,default='test')
+args = parser.parse_args()
+print(args)
 
-if model == 'gpt2':
-   file = [json.loads(l) for l in open('results_' + subset + '.json').readlines()] #[ast.literal_eval(l.strip().replace("'", '"')) for l in open('results_' + subset + '.json').readlines()]
-else:
-   file = [json.loads(l) for l in open('results_lstm_' + subset + '.json').readlines()]
+subset = args.subset 
+gen_folder = args.gen_folder
+source_folder = args.source_folder
+
+file = [json.loads(l) for l in open('results_' + subset + '.json').readlines()]
 
 delta = .5
 folders = ['org','adj','cov','order']
@@ -27,16 +34,14 @@ for f in file:
     adj_a = np.argmax(list( map(add, [l * delta for l in f['prob']],[l* delta for l in f['adj']])))
     cov_a = np.argmax(list( map(add, [l * delta for l in f['prob']],[l* delta for l in f['cov']])))
     order_a = np.argmax(list( map(add, [l * delta for l in f['prob']],[l* delta for l in f['order']])))
-    if model == 'gpt2':
-       fact_a = np.argmax(list( map(add, [l * delta for l in f['prob']],[l* delta for l in f['fact']])))
-       f_fact = f['files'][fact_a]
+    fact_a = np.argmax(list( map(add, [l * delta for l in f['prob']],[l* delta for l in f['fact']])))
+    f_fact = f['files'][fact_a]
     f_prob = f['files'][prob_a]
     f_adj = f['files'][adj_a]
     f_cov = f['files'][cov_a]
     f_order = f['files'][order_a]
     retrieved = [f_prob,f_adj,f_cov,f_order]
-    if model == 'gpt2':
-       retrieved.append(f_fact)
+    retrieved.append(f_fact)
     for r in range(len(retrieved)):
         summ = json.load(open(source_folder + '/' + retrieved[r]))
         id = f['id']
